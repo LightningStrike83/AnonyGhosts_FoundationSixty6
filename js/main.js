fdfd3942b031c0bdcf841b59ea23c3af7102a24a
Dan Singer <d_singer@fanshaweonline.ca>
2024-01-06 19:37:49 -0500

    Header Initial Code
    Added some initial code for the header

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 (() => {
+    const hamMenu = document.querySelector("#hamburger-menu-icon")
 
-    
+    function openMobileNav() {
+        var headerMenu = document.querySelector("")
+
+        if (headerMenu.style.display === "grid") {
+            headerMenu.style.display = "none"
+        } else {
+            headerMenu.style.display = "grid"
+        }
+    }
+
+    hamMenu.addEventListener("click", openMobileNav)
+
 })();
