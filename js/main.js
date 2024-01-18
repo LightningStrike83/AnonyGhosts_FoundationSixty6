cfd7ef7041c4bf0d565f0774697986aeb55b6abe
Dan Singer <d_singer@fanshaweonline.ca>
2024-01-18 00:31:19 -0500

    Drop Menu and Temporary Pages
    Added drop menu functionality and temporary pages (to be discussed during meeting)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 (() => {
     const hamMenu = document.querySelector("#hamburger-menu-icon")
+    const dropMenu = document.querySelectorAll(".drop-menu")
+    const expandedMenu = document.querySelectorAll(".expand-menu")
 
     function openMobileNav() {
         var headerMenu = document.querySelector("#nav-menu")
@@ -11,6 +13,61 @@
         }
     }
 
-    hamMenu.addEventListener("click", openMobileNav)
+    function openMenu() {
+        if (this.id === "about-nav") {
+            const aboutMenu = document.querySelector("#about-menu")
 
+            if (aboutMenu.style.display === "flex") {
+                aboutMenu.style.display = "none"
+            } else {
+                expandedMenu.forEach(menu =>{
+                    menu.style.display = "none"
+                })
+                aboutMenu.style.display = "flex"
+            }
+
+            
+        } else if (this.id === "help-nav") {
+            const getHelpMenu = document.querySelector("#get-help-menu")
+
+            if (getHelpMenu.style.display === "flex") {
+                getHelpMenu.style.display = "none"
+            } else {
+                expandedMenu.forEach(menu =>{
+                    menu.style.display = "none"
+                })
+                getHelpMenu.style.display = "flex"
+            }
+
+            
+        } else if (this.id === "involved-nav") {
+            const involvedMenu = document.querySelector("#get-involved-menu")
+
+            if (involvedMenu.style.display === "flex") {
+                involvedMenu.style.display = "none"
+            } else {
+                expandedMenu.forEach(menu =>{
+                    menu.style.display = "none"
+                })
+                involvedMenu.style.display = "flex"
+            }
+        }
+    }
+
+    function openAbout() {
+        let aboutMenu = document.querySelector("#about-menu")
+
+        expandedMenu.forEach(menu =>{
+            menu.style.display = "none"
+        })
+
+        aboutMenu.style.display = "flex"
+    }
+
+    dropMenu.forEach(drop => drop.addEventListener("click", openMenu))
+
+    hamMenu.addEventListener("click", openMobileNav)
+    //aboutNav.addEventListener("click", openAbout)
+    //helpNav.addEventListener("click", openHelp)
+    //involvedNav.addEventListener("click", openInvolved)
 })();
