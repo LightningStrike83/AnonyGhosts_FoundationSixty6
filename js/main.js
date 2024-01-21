5799c73985be759e8afe00c2c4526ce83362f10f
Dan Singer <d_singer@fanshaweonline.ca>
2024-01-20 23:48:38 -0500

    Bottom Menu
    Got the bottom menu to function

--- js/main.js
+++ js/main.js
@@ -2,6 +2,19 @@
     const hamMenu = document.querySelector("#hamburger-menu-icon")
     const dropMenu = document.querySelectorAll(".drop-menu")
     const expandedMenu = document.querySelectorAll(".expand-menu")
+    const bottomMenu = document.querySelector("#bottom-menu")
+    const activateButton = document.querySelector("#activation-button")
+    const activateImage = document.querySelector("#activation-button img")
+
+    let iconpath = [
+        {
+            path: "dark-mode"
+        },
+
+        {
+            path: "emergency-phone"
+        }
+    ]
 
     function openMobileNav() {
         var headerMenu = document.querySelector("#nav-menu")
@@ -64,9 +77,59 @@
         aboutMenu.style.display = "flex"
     }
 
+    function openBottomMenu() {
+        if (this.id === "activation-button") {
+            let buttonTemplate = document.querySelector("#button-template")
+
+            activateButton.classList.remove("clicked")
+            activateImage.src = "images/icons/x-icon.svg"
+            activateButton.setAttribute("id", "close-button")
+
+            iconpath.forEach(info => {
+                const clone = buttonTemplate.content.cloneNode(true);
+                const cloneDiv = clone.querySelector(".clone-div")
+                const cloneIMG = clone.querySelector(".clone-img")
+
+                cloneDiv.setAttribute("id", `${info.path}`)
+                cloneIMG.setAttribute("class", "bottom-icon")
+                cloneIMG.setAttribute("alt", `${info.path} Icon`)
+                cloneIMG.src = `images/icons/${info.path}.svg`
+
+                bottomMenu.appendChild(clone)
+            })
+
+            {
+                let darkButton = document.querySelector("#dark-mode")
+                let phoneButton = document.querySelector("#emergency-phone")
+
+                function darkMode() {
+
+                }
+
+                function openNumbers() {
+
+                }
+
+                darkButton.addEventListener("click", darkMode)
+                phoneButton.addEventListener("click", openNumbers)
+            }
+        } else {
+            const closeButton = document.querySelector("#close-button")
+            const cloneDivs = document.querySelectorAll(".clone-div")
+
+            activateImage.src = "images/icons/image-icon.svg"
+
+            closeButton.setAttribute("id", "activation-button")
+
+            cloneDivs.forEach(clone =>
+                clone.innerHTML = "")
+        }
+    }
+
     dropMenu.forEach(drop => drop.addEventListener("click", openMenu))
 
     hamMenu.addEventListener("click", openMobileNav)
+    activateButton.addEventListener("click", openBottomMenu)
     //aboutNav.addEventListener("click", openAbout)
     //helpNav.addEventListener("click", openHelp)
     //involvedNav.addEventListener("click", openInvolved)
